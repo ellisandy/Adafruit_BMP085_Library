439b024f55115ebf45ea46a32506fd62b6934441
Jack Ellis <jack@mnmlst.cc>
2016-10-06 08:34:47 -0700

    Revert "Adafruit fix for Particle"
    This reverts commit eb8bb0c5b7fec7da3e30af5b399179342c9a4983.

--- firmware/Adafruit_BMP085.h
+++ firmware/Adafruit_BMP085.h
@@ -1,27 +1,35 @@
-/***************************************************
+//-----------------------------------------------------------------
+// BMP085/BMP180 Barometric Pressure + Temp sensor for Spark Core
+//=================================================================
+// Copy this into a new application at:
+// https://www.spark.io/build and go nuts!
+//-----------------------------------------------------------------
+// Technobly / BDub - Jan 2014
+//=================================================================
+
+/*************************************************** 
   This is a library for the Adafruit BMP085/BMP180 Barometric Pressure + Temp sensor
 
-  Designed specifically to work with the Adafruit BMP085 or BMP180 Breakout
+  Designed specifically to work with the Adafruit BMP085 or BMP180 Breakout 
   ----> http://www.adafruit.com/products/391
   ----> http://www.adafruit.com/products/1603
 
-  These displays use I2C to communicate, 2 pins are required to
+  These displays use I2C to communicate, 2 pins are required to  
   interface
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
   products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 #include <math.h>
 
-#define boolean bool
-
+#define boolean bool 
+  
 #define BMP085_DEBUG 0
 
-//#define BMP085_I2CADDR 0x77
-#define BMP085_I2CADDR 0xEE
+#define BMP085_I2CADDR 0x77 // Change back to 0x77 when Spark I2C library uses 7-bit addressing
 
 #define BMP085_ULTRALOWPOWER 0
 #define BMP085_STANDARD      1
@@ -29,7 +37,7 @@
 #define BMP085_ULTRAHIGHRES  3
 #define BMP085_CAL_AC1           0xAA  // R   Calibration data (16 bits)
 #define BMP085_CAL_AC2           0xAC  // R   Calibration data (16 bits)
-#define BMP085_CAL_AC3           0xAE  // R   Calibration data (16 bits)
+#define BMP085_CAL_AC3           0xAE  // R   Calibration data (16 bits)    
 #define BMP085_CAL_AC4           0xB0  // R   Calibration data (16 bits)
 #define BMP085_CAL_AC5           0xB2  // R   Calibration data (16 bits)
 #define BMP085_CAL_AC6           0xB4  // R   Calibration data (16 bits)
@@ -39,7 +47,7 @@
 #define BMP085_CAL_MC            0xBC  // R   Calibration data (16 bits)
 #define BMP085_CAL_MD            0xBE  // R   Calibration data (16 bits)
 
-#define BMP085_CONTROL           0xF4
+#define BMP085_CONTROL           0xF4 
 #define BMP085_TEMPDATA          0xF6
 #define BMP085_PRESSUREDATA      0xF6
 #define BMP085_READTEMPCMD       0x2E
@@ -55,7 +63,7 @@ class Adafruit_BMP085 {
   float readAltitude(float sealevelPressure = 101325); // std atmosphere
   uint16_t readRawTemperature(void);
   uint32_t readRawPressure(void);
-
+  
  private:
   uint8_t read8(uint8_t addr);
   uint16_t read16(uint8_t addr);
